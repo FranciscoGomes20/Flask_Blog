93e65855dace07b03633b717a0cb3d2aafdc5bd5
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-02-17 07:57:29 -0500

    Inicio do suporte por e-mail

--- blog.py
+++ blog.py
@@ -3,7 +3,8 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
- 
+from flask_mail import Mail
+
 app = Flask(__name__)
 
 login = LoginManager(app)
@@ -13,5 +14,6 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app import routes, errors, models
