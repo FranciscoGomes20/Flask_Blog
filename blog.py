856cd40a7e902199e2af6f77c96069366a604260
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-03-10 14:49:40 -0500

    Inicio do frontend

--- blog.py
+++ blog.py
@@ -1,9 +1,11 @@
+from ensurepip import bootstrap
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -15,5 +17,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, errors, models
