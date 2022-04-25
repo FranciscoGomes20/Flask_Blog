5404e7bab69f02a9b0fd3bc40cb4d113c7d43cf5
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-04-25 14:59:32 -0500

    Formato de data e hora

--- blog.py
+++ blog.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
@@ -18,5 +19,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, errors, models
