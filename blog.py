23c5e0d4885755592401967590d75fc72cd4ff67
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-05-17 16:46:17 -0500

    Inicio do flask-babel

--- blog.py
+++ blog.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 
@@ -19,5 +21,10 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, errors, models
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
